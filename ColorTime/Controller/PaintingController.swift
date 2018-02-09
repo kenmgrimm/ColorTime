c30e1041dea1f90106886adf34e374347144171a
Ken Grimm <kenmgrimm@gmail.com>
2018-02-09 11:23:49 -0700

    Appears to work on all form factors + allows for device rotation.  Displays constraint breaking message on rotation though so that does need to be sorted.

--- ColorTime/Controller/PaintingController.swift
+++ ColorTime/Controller/PaintingController.swift
@@ -3,22 +3,31 @@ import UIKit
 class PaintingController : UIViewController, UIGestureRecognizerDelegate {
   @IBOutlet var scrollView: UIScrollView!
 
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
+    print(#function)
+
     scrollView.delegate = self
     
     let imageView = PaintingImageView()
-    imageView.image = #imageLiteral(resourceName: "owls")
+    //    imageView.image = #imageLiteral(resourceName: "owls")
+    imageView.image = #imageLiteral(resourceName: "tiger")
     imageView.contentMode = .scaleAspectFit
     
     imageView.sizeToFit()  // Size the imageView to fit the image
-
+    
     scrollView.flashScrollIndicators()
-
+    
     scrollView.addSubview(imageView)
-
+    
     scrollView.contentSize = imageView.frame.size
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    
+    print(#function)
   }
   
   override func viewDidAppear(_ animated: Bool) {
@@ -38,6 +47,14 @@ class PaintingController : UIViewController, UIGestureRecognizerDelegate {
     // Re-center (later)
     //    scrollView.contentOffset = CGPoint(x: -20, y: -20)
   }
+  
+//  override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//    print("\(#function): \(size)")
+//  }
+//  
+//  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+//    print("\(#function): \(previousTraitCollection?.verticalSizeClass == .compact), \(previousTraitCollection?.horizontalSizeClass == .compact)")
+//  }
 }
 
 extension PaintingController : UIScrollViewDelegate {
